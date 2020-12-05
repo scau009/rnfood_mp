3908f26526cd186fb349524f35ecef02e1d22e1e
bear <18819259245@163.com>
2020-12-05 20:52:50 +0800

    init

---
+++ src/app.config.js
@@ -0,0 +1,15 @@
+export default {
+  pages: [
+    'pages/index/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  }
+}
+
+
+//AppSecret = c584cce43601f1d05ca15d8690f7fdda
+//AppID = wx857daea9c9c24bfc
