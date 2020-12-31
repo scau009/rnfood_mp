24e9f28dbe1dba6299a3ef74605b5d753703e079
bear <18819259245@163.com>
2020-12-31 13:35:16 +0800

    第一个版本

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { View, Image} from '@tarojs/components'
-import {AtButton} from "taro-ui";
+import {AtButton, AtTabs, AtTabsPane} from "taro-ui";
 
 import {connect} from "react-redux";
 import Taro from "@tarojs/taro";
@@ -9,6 +9,8 @@ import {LOGIN} from "../../constants/auth";
 import LoginPage from "../compoment/login";
 import coupons from "../../asset/image/coupons.png";
 import orders from "../../asset/image/orders.png";
+import TradeItem from "../compoment/trade";
+import Api from "../../apiClient/apiClient";
 
 
 @connect(({ loginUpdater }) => ({
@@ -20,20 +22,144 @@ import orders from "../../asset/image/orders.png";
 }))
 class Index extends Component {
 
-  constructor(props) {
-    super(props);
+  constructor () {
+    super(...arguments);
     this.state = {
-    };
-  }
+      current: 0,
+      tabList: [{title: '全部'}, {title: '待支付'}, {title: '已支付'},{title:'已完成'},{title:'已取消'}],
 
+      allTradeList: [],
+      page_all: 1,
+      end_all: false,
 
-  componentWillMount() {
-    if (this.props.loginUpdater.isLogin) {
+      waitPayTradeList: [],
+      page_wait_pay: 1,
+      end_wait_pay: false,
 
+      paidTradeList: [],
+      page_paid: 1,
+      end_paid: false,
+
+      finishedTradeList: [],
+      page_finished: 1,
+      end_finished: false,
+
+      canceledTradeList: [],
+      page_cancel: 1,
+      end_cancel: false,
     }
   }
 
-  componentWillUnmount () { }
+  handleClick (value) {
+    this.setState({
+      current: value
+    },()=>{
+      const {tradeList} = {...this.getStatusAndPage()};
+      if (tradeList.length <= 0) {
+        this.getList(this.state.current);
+      }
+    })
+  }
+
+  componentWillMount() {
+    this.getList(this.state.current)
+  }
+
+  onReachBottom() {
+    this.getList(this.state.current)
+  }
+
+  getStatusAndPage() {
+    let status = '';
+    let page = this.state.page_all;
+    let end = false;
+    let tradeList = [];
+    switch (this.state.current) {
+      case 0:
+        status = '';
+        page = this.state.page_all;
+        end = this.state.end_all;
+        tradeList = this.state.allTradeList;
+        break;
+      case 1:
+        status = 'wait_pay';
+        page = this.state.page_wait_pay;
+        end = this.state.end_wait_pay;
+        tradeList = this.state.waitPayTradeList;
+        break;
+      case 2:
+        status = 'paid';
+        page = this.state.page_paid;
+        end = this.state.end_paid;
+        tradeList = this.state.paidTradeList;
+        break;
+      case 3:
+        status = 'finished';
+        page = this.state.page_finished;
+        end = this.state.end_finished;
+        tradeList = this.state.finishedTradeList;
+        break;
+      case 4:
+        status = 'canceled';
+        page = this.state.page_cancel;
+        end = this.state.end_cancel;
+        tradeList = this.state.canceledTradeList;
+        break;
+      default:
+        status = '';
+    }
+    return {status:status,page:page,end:end,tradeList:tradeList};
+  }
+
+  getList(index) {
+    const {status, page, end} = {...this.getStatusAndPage()};
+    if (end) {
+      return;
+    }
+    Api.request("GET", "/api/trades/list", {page: page, status: status}).then((res) => {
+      switch (index) {
+        case 0:
+          this.setState({
+            allTradeList: this.state.allTradeList.concat(res.list),
+            page_all: res.paginate.hasNext ? res.paginate.page + 1 : res.paginate.page,
+            end_all: !res.paginate.hasNext
+          });
+          break;
+        case 1:
+          this.setState({
+            waitPayTradeList: this.state.waitPayTradeList.concat(res.list),
+            page_wait_pay: res.paginate.hasNext ? res.paginate.page + 1 : res.paginate.page,
+            end_wait_pay: !res.paginate.hasNext
+          });
+          break;
+        case 2:
+          this.setState({
+            paidTradeList: this.state.paidTradeList.concat(res.list),
+            page_paid: res.paginate.hasNext ? res.paginate.page + 1 : res.paginate.page,
+            end_paid: !res.paginate.hasNext
+          });
+          break;
+        case 3:
+          this.setState({
+            finishedTradeList: this.state.finishedTradeList.concat(res.list),
+            page_finished: res.paginate.hasNext ? res.paginate.page + 1 : res.paginate.page,
+            end_finished: !res.paginate.hasNext
+          });
+          break;
+        case 4:
+          this.setState({
+            canceledTradeList: this.state.canceledTradeList.concat(res.list),
+            page_cancel: res.paginate.hasNext ? res.paginate.page + 1 : res.paginate.page,
+            end_cancel: !res.paginate.hasNext
+          });
+          break;
+        default:
+
+      }
+
+    });
+  }
+
 
   handleGoToTrades() {
     Taro.navigateTo({
@@ -44,6 +170,8 @@ class Index extends Component {
 
   render () {
     const {avatarUrl,nickName,mobile} = {...this.props.loginUpdater.userInfo};
+    const {tabList,allTradeList,waitPayTradeList,paidTradeList,finishedTradeList,canceledTradeList} = {...this.state};
+
     return (
       <View className='bg-default vh-100'>
         {
@@ -60,24 +188,73 @@ class Index extends Component {
               </View>
             </View>
 
-            <View className='content'>
-              <View className='trade_block'>
-                <View className='at-row at-row__align-content--center'>
-                  <View className='at-col text-center'>
-                    <View>
-                      <Image src={coupons} className='user_icon' />
-                    </View>
-                    <View className='text-mute text-small'>优惠券</View>
-                  </View>
-                  <View className='at-col text-center' onClick={this.handleGoToTrades.bind(this)}>
-                    <View>
-                      <Image src={orders} className='user_icon' />
-                    </View>
-                    <View className='text-mute text-small'>全部订单</View>
-                  </View>
+            {/*<View className='content'>*/}
+            {/*  <View className='trade_block'>*/}
+            {/*    <View className='at-row at-row__align-content--center'>*/}
+            {/*      <View className='at-col text-center'>*/}
+            {/*        <View>*/}
+            {/*          <Image src={coupons} className='user_icon' />*/}
+            {/*        </View>*/}
+            {/*        <View className='text-mute text-small'>优惠券</View>*/}
+            {/*      </View>*/}
+            {/*      <View className='at-col text-center' onClick={this.handleGoToTrades.bind(this)}>*/}
+            {/*        <View>*/}
+            {/*          <Image src={orders} className='user_icon' />*/}
+            {/*        </View>*/}
+            {/*        <View className='text-mute text-small'>全部订单</View>*/}
+            {/*      </View>*/}
+            {/*    </View>*/}
+            {/*  </View>*/}
+            {/*</View>*/}
+            <View className='post_top'>
+            {this.props.loginUpdater.isLogin ? <AtTabs current={this.state.current} tabList={tabList} onClick={this.handleClick.bind(this)}>
+              <AtTabsPane current={this.state.current} index={0} >
+                <View className='bg-default list_page' >
+                  {
+                    allTradeList.length>0 && allTradeList.map((trade)=>{
+                      return <TradeItem trade={trade} />;
+                    })
+                  }
                 </View>
-              </View>
-            </View>
+              </AtTabsPane>
+              <AtTabsPane current={this.state.current} index={1}>
+                <View className='bg-default list_page' >
+                  {
+                    waitPayTradeList.length>0 && waitPayTradeList.map((trade)=>{
+                      return <TradeItem trade={trade} />;
+                    })
+                  }
+                </View>
+              </AtTabsPane>
+              <AtTabsPane current={this.state.current} index={2}>
+                <View className='bg-default list_page' >
+                  {
+                    paidTradeList.length>0 && paidTradeList.map((trade)=>{
+                      return <TradeItem trade={trade} />;
+                    })
+                  }
+                </View>
+              </AtTabsPane>
+              <AtTabsPane current={this.state.current} index={3}>
+                <View className='bg-default list_page' >
+                  {
+                    finishedTradeList.length>0 && finishedTradeList.map((trade)=>{
+                      return <TradeItem trade={trade} />;
+                    })
+                  }
+                </View>
+              </AtTabsPane>
+              <AtTabsPane current={this.state.current} index={4}>
+                <View className='bg-default list_page' >
+                  {
+                    canceledTradeList.length>0 && canceledTradeList.map((trade)=>{
+                      return <TradeItem trade={trade} />;
+                    })
+                  }
+                </View>
+              </AtTabsPane>
+            </AtTabs> : <LoginPage />}
+          </View>
           </View> : <LoginPage />
         }
       </View>
